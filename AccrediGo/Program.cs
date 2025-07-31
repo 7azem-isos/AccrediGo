9dd9eeaa8ec854151b0b0bd0fc387e9c48a7d915
akhalil-isos <abdelrahman@isosmena.com>
2025-07-31 11:14:32 +0300

    Update Program.cs

--- AccrediGo/Program.cs
+++ AccrediGo/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using AccrediGo.Infrastructure.Data;
 
 namespace AccrediGo
 {
@@ -10,6 +12,8 @@ namespace AccrediGo
             // Add services to the container.
 
             builder.Services.AddControllers();
+            builder.Services.AddDbContext<AccrediGoDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
