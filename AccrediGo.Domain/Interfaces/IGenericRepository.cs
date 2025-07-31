ca9059896b201e19d777b9ffa2f0595d14e1f899
akhalil-isos <abdelrahman@isosmena.com>
2025-07-31 12:11:38 +0300

    Added IGenericRepository and GenericRepository, with dynamic key retrieval

---
+++ AccrediGo.Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,29 @@
+﻿using AccrediGo.Domain.Entities.BaseModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccrediGo.Domain.Interfaces
+{
+    public interface IGenericRepository<TEntity> where TEntity : BaseEntity
+    {
+        Task<TEntity> GetByIdAsync(string id, params Expression<Func<TEntity, object>>[] includes);
+    Task<IEnumerable<TEntity>> GetAllAsync(
+        Expression<Func<TEntity, bool>>? filter = null,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
+        params Expression<Func<TEntity, object>>[] includes);
+    Task<IEnumerable<TEntity>> FindAsync(
+        Expression<Func<TEntity, bool>> filter,
+        params Expression<Func<TEntity, object>>[] includes);
+    Task AddAsync(TEntity entity);
+    Task AddRangeAsync(IEnumerable<TEntity> entities);
+    void Update(TEntity entity);
+    void UpdateRange(IEnumerable<TEntity> entities);
+    Task SoftDeleteAsync(string id);
+    Task<bool> ExistsAsync(string id);
+    Task<int> SaveChangesAsync();
+}
+}
